ad6425c932146f73b867c282558f28e78c6f8539
santos <santoslopez890@gmail.com>
2023-08-18 10:28:52 -0400

    done presenting

--- src/App.js
+++ src/App.js
@@ -1,64 +1,42 @@
  import logo from './logo.svg';
-// import ImageSlider from "./ImageSlider";
+
  import './App.css';
-
-// function App() {
-//   const slides = [
-//     { url: "sliderapp/src/pictures/download (1).jpeg", title: "beach" },
-//     { url: "sliderapp/src/pictures/download.jpeg", title: "boat" },
-//     { url: "/Users/santos/LocalPros/REACT JS /sliderapp/src/pictures/Red_Panda_(24986761703).jpg", title: "forest" },
-//   ];
-//   const containerStyles = {
-//     width: "500px",
-//     height: "280px",
-//     margin: "0 auto",
-//   };
-//   return (
-//     <div className="App">
-//       <header className="App-header">
-//         <img src={logo} className="App-logo" alt="logo" />
-//         <p>
-//           Edit <code>src/App.js</code> and save to reload.
-//         </p>
-//         <a
-//           className="App-link"
-//           href="https://reactjs.org"
-//           target="_blank"
-//           rel="noopener noreferrer"
-//         >
-//           Learn React with Santos
-//           <container style={containerStyles}>
-//         <ImageSlider slides={slides} />
-//       </container>
-//         </a>
-//       </header>
-//     </div>
-//   );
-// }
-
-// export default App;
 import ImageSlider from "./ImageSlider";
+import React from 'react';
 const App = () => {
   const slides = [
-    { url: "http://localhost:3000/image1.jpeg", title: "beach" },
-    { url: "http://localhost:3000/image2.jpeg", title: "boat" },
-    { url: "http://localhost:3000/image3.jpg", title: "forest" },
+    { url: "http://localhost:3000/image1.png", title: "slide1" },
+    { url: "http://localhost:3000/image2.png", title: "slide2" },
+    { url: "http://localhost:3000/image3.png", title: "slide3" },
+    { url: "http://localhost:3000/image4.png", title: "slide4" },
+    { url: "http://localhost:3000/image5.png", title: "slide5" },
+    { url: "http://localhost:3000/image6.png", title: "slide6" },
+    { url: "http://localhost:3000/image7.png", title: "slide7" },
+    { url: "http://localhost:3000/image8.png", title: "slide8" },
+    { url: "http://localhost:3000/image9.png", title: "slide9" },
+    { url: "http://localhost:3000/image10.png", title: "slide10" },
+    { url: "http://localhost:3000/image13.jpg", title: "slide13" },
   ];
+
   const containerStyles = {
-    width: "1000px",
-    height: "500px",
+    width: "1300px",
+    height: "650px",
     margin: "0 auto",
   };
   return (
     <div className="App">
       <header className="App-header">
          <img src={logo} className="App-logo" alt="logo" />
-      <p>Learning REACT</p>
+         
+        
       <div style={containerStyles}>
         <ImageSlider slides={slides} />
       </div>
+    
       </header>
+      
     </div>
+    
   );
 };
 
--- src/ImageSlider.js
+++ src/ImageSlider.js
@@ -6,6 +6,7 @@ const slideStyles = {
   borderRadius: "10px",
   backgroundSize: "cover",
   backgroundPosition: "center",
+    
 };
 
 const rightArrowStyles = {
@@ -14,7 +15,18 @@ const rightArrowStyles = {
   transform: "translate(0, -50%)",
   right: "32px",
   fontSize: "45px",
-  color: "#fff",
+  color: "#aaa",
+  zIndex: 1,
+  cursor: "pointer",
+};
+
+const textStyle = {
+  position: "center",
+  top: "50%",
+  transform: "translate(0, -50%)",
+  right: "32px",
+  fontSize: "45px",
+  color: "#aaa",
   zIndex: 1,
   cursor: "pointer",
 };
@@ -25,7 +37,7 @@ const leftArrowStyles = {
   transform: "translate(0, -50%)",
   left: "32px",
   fontSize: "45px",
-  color: "#fff",
+  color: "#aaa",
   zIndex: 1,
   cursor: "pointer",
 };
@@ -46,6 +58,7 @@ const dotStyle = {
   fontSize: "20px",
 };
 
+
 const ImageSlider = ({ slides }) => {
   const [currentIndex, setCurrentIndex] = useState(0);
   const goToPrevious = () => {
@@ -53,6 +66,12 @@ const ImageSlider = ({ slides }) => {
     const newIndex = isFirstSlide ? slides.length - 1 : currentIndex - 1;
     setCurrentIndex(newIndex);
   };
+  function handleClick() {
+    // Redirect to the desired URL
+    if(currentIndex==5){
+    window.location.href = 'https://codepen.io/gaearon/pen/gWWZgR?editors=0010';
+    }
+  }
   const goToNext = () => {
     const isLastSlide = currentIndex === slides.length - 1;
     const newIndex = isLastSlide ? 0 : currentIndex + 1;
@@ -65,18 +84,20 @@ const ImageSlider = ({ slides }) => {
     ...slideStyles,
     backgroundImage: `url(${slides[currentIndex].url})`,
   };
-
   return (
     <div style={sliderStyles}>
       <div>
         <div onClick={goToPrevious} style={leftArrowStyles}>
-          ❰
+          ❰ 
         </div>
         <div onClick={goToNext} style={rightArrowStyles}>
           ❱
         </div>
       </div>
-      <div style={slideStylesWidthBackground}></div>
+
+      <div style={slideStylesWidthBackground} onClick={handleClick}>
+
+      </div>
       <div style={dotsContainerStyles}>
         {slides.map((slide, slideIndex) => (
           <div
